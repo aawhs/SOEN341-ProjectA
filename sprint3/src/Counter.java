deaad8fb1605d410bf532670158aef793a2a70eb
aawhs <ahmed.ali@mail.concordia.ca>
2020-10-14 15:46:01 -0400

    added abstract process method to abstract class

--- sprint3/src/Counter.java
+++ sprint3/src/Counter.java
@@ -1,7 +1,12 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 abstract class Counter implements ICounter {
     Integer count = 0;
     boolean whiteSpace = false;
 
+    abstract public void process(ArrayList<IFileManager> file, IOption opt) throws IOException, URISyntaxException;
     public Integer getCount() {
         return this.count;
     }
