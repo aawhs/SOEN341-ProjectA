8c082def75a64f7c7580173098cb942f5c786278
aawhs <ahmed.ali@mail.concordia.ca>
2020-10-14 02:18:41 -0400

    Implemented a more efficient and enhanced File Parsing method for all counters & Added absolute paths & program names to outputs

--- sprint3/src/ICounter.java
+++ sprint3/src/ICounter.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 public interface ICounter {
     int EOF = -1;
-    void process(IFilePath file, IOption dst)throws IOException;
-    void process(IFilePath file, Object dst, IOption opt)throws IOException;
+    void process(ArrayList<IFileManager> file, IOption dst) throws IOException, URISyntaxException;
     Integer getCount();
 }
 
