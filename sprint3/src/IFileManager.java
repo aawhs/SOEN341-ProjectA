c8bd70c1979dbe6ca855ca7c6d684f75bc118f4b
aawhs <ahmed.ali@mail.concordia.ca>
2020-10-17 16:31:24 -0400

    No changes

--- sprint3/src/IFileManager.java
+++ sprint3/src/IFileManager.java
@@ -1,23 +1,17 @@
 import java.io.*;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public interface IFileManager {
     boolean canReadFile();
     boolean isOpen();
     void setFilePath() throws URISyntaxException;
     void openFile() throws URISyntaxException;
+    File getFile();
     void openInputStream() throws FileNotFoundException, URISyntaxException;
     void openOutputStream() throws FileNotFoundException, URISyntaxException;
     void close() throws IOException;
-    void reset() throws IOException, URISyntaxException;
     void dstFilePath();
     FileInputStream getFileInStream();
     FileOutputStream getFileOutStream();
-    void setBufferedReader() throws FileNotFoundException;
-    BufferedReader getBufferedReader();
-
 }
 
