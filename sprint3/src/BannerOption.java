470e20d5d8abd323ebe79d5962cd02ed9f9d0c37
aawhs <ahmed.ali@mail.concordia.ca>
2020-10-17 16:27:12 -0400

    added setUsgae() method to Interface Option and subclasses

--- sprint3/src/BannerOption.java
+++ sprint3/src/BannerOption.java
@@ -1,6 +1,6 @@
 import static java.lang.System.exit;
 
-public class BannerOption extends Option {
+class BannerOption extends Option {
     public BannerOption() {
         super(new String[]{"-b"},"-banner");
         setEnable(true);
@@ -8,10 +8,33 @@ public class BannerOption extends Option {
 
     @Override
     public void process() {
-        System.out.println("wcOO "+ getClassName() + " Version 1.2\n" +
-                "Copyright (C) Concordia University 2020. All Rights Reserved.\n" +
-                "Written by Ahmed Ali\n");
-
+        switch (getClassName()){
+            case "Copy":
+                System.out.println("wcOO copy Version 2.0\n" +
+                        "Copyright (C) Concordia University 2020. All Rights Reserved.\n" +
+                        "Written by TEAM C3\n");
+                break;
+            case "CharCounter":
+                System.out.println("wcOO charcount Version 2.0\n" +
+                        "Copyright (C) Concordia University 2020. All Rights Reserved.\n" +
+                        "Written by TEAM C3\n");
+                break;
+            case "WordCounter":
+                System.out.println("wcOO wordcount Version 2.0\n" +
+                        "Copyright (C) Concordia University 2020. All Rights Reserved.\n" +
+                        "Written by TEAM C3\n");
+                break;
+            case "LineCounter":
+                System.out.println("wcOO linecount Version 2.0\n" +
+                        "Copyright (C) Concordia University 2020. All Rights Reserved.\n" +
+                        "Written by TEAM C3\n");
+                break;
+            default:
+                System.out.println("wcOO Version 2.0\n" +
+                        "Copyright (C) Concordia University 2020. All Rights Reserved.\n" +
+                        "Written by TEAM C3\n");
+                break;
+        }
         exit(1);
     }
 
